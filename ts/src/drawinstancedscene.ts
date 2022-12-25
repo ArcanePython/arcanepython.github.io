7193be3ff457590890cbada2e726b1d5ad9ed801
ArcanePython <81058956+ArcanePython@users.noreply.github.com>
2022-12-25 13:36:17 +0100

    Christmas update

--- ts/src/drawinstancedscene.ts
+++ ts/src/drawinstancedscene.ts
@@ -61,138 +61,122 @@ viewLoc: WebGLUniformLocation | undefined;
 vao: WebGLVertexArrayObject | undefined;
 
 numVertices:number = 12;
-numInstances: number = 5;
+numInstances: number = 6;
 matrices: Float32Array[] = [];
 matrixBuffer: WebGLBuffer | undefined;
 matrixData: Float32Array| undefined;
 
-//public resizeCanvas(gl: WebGL2RenderingContext) { twgl.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement); }
+  initScene(gl: WebGL2RenderingContext, cap: scene.TAnimation1Parameters,  p: twgl.ProgramInfo) 
+  {
+    twgl.setAttributePrefix("a_");
+  
+    this.gl = gl;
+    this.program = p.program;
+    //const programInfo = twgl.createProgramInfo(gl, [this.vertexShaderSource, this.fragmentShaderSource]);
+    //this.program = programInfo.program;
 
-initScene(gl: WebGL2RenderingContext, cap: scene.TAnimation1Parameters,  p: twgl.ProgramInfo) 
-{
-  twgl.setAttributePrefix("a_");
- 
-  this.gl = gl;
-  this.program = p.program;
-  //const programInfo = twgl.createProgramInfo(gl, [this.vertexShaderSource, this.fragmentShaderSource]);
-  //this.program = programInfo.program;
+    gl.useProgram(this.program);
 
-  gl.useProgram(this.program);
+    const positionLoc = gl.getAttribLocation(this.program, 'a_position');
+    const colorLoc = gl.getAttribLocation(this.program, 'color');
+    const matrixLoc = gl.getAttribLocation(this.program, 'matrix');
+    this.projectionLoc = gl.getUniformLocation(this.program, 'projection')!;
+    this.viewLoc = gl.getUniformLocation(this.program, 'view')!;
 
-  const positionLoc = gl.getAttribLocation(this.program, 'a_position');
-  const colorLoc = gl.getAttribLocation(this.program, 'color');
-  const matrixLoc = gl.getAttribLocation(this.program, 'matrix');
-  this.projectionLoc = gl.getUniformLocation(this.program, 'projection')!;
-  this.viewLoc = gl.getUniformLocation(this.program, 'view')!;
+    // Create a vertex array object (attribute state)
+    this.vao = gl.createVertexArray()!;
 
-  // Create a vertex array object (attribute state)
-  this.vao = gl.createVertexArray()!;
+    // and make it the one we're currently working with
+    gl.bindVertexArray(this.vao);
 
-  // and make it the one we're currently working with
-  gl.bindVertexArray(this.vao);
-
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    /*     -0.1,  0.4,
-      -0.1, -0.4,
-       0.1, -0.4,
-       0.1, -0.4,
-      -0.1,  0.4,
-       0.1,  0.4,
-       0.4, -0.1,
-      -0.4, -0.1,
-      -0.4,  0.1,
-      -0.4,  0.1,
-       0.4, -0.1,
-       0.4,  0.1, */  // Lx Corrected Greggs coords list to have the triangles of each spoke turn in the same direction.
-                     // This allowed  gl.enable(gl.DEPTH_TEST) and gl.enable(gl.CULL_FACE) in the
-                     // preparation of wrapper animation1.Animation1.
-
-                     -0.1,  0.4,
-                     -0.1, -0.4,
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+      
+                      -0.1,  0.4,
+                      -0.1, -0.4,
+                        0.1, -0.4,
+                      -0.1,  0.4,
                       0.1, -0.4,
-                     -0.1,  0.4,
-                     0.1, -0.4,
-                     0.1,  0.4, 
-                     -0.4, -0.1,
-                     0.4, -0.1,
-                     -0.4,  0.1,
-                     -0.4,  0.1,
+                      0.1,  0.4, 
+                      -0.4, -0.1,
                       0.4, -0.1,
-                      0.4,  0.1, 
-               
-    ]), gl.STATIC_DRAW);
-  this.numVertices = 12;
+                      -0.4,  0.1,
+                      -0.4,  0.1,
+                        0.4, -0.1,
+                        0.4,  0.1, 
+                
+      ]), gl.STATIC_DRAW);
+    this.numVertices = 12;
 
-  // setup the position attribute
-  gl.enableVertexAttribArray(positionLoc);
-  gl.vertexAttribPointer(
-      positionLoc,  // location
-      2,            // size (num values to pull from buffer per iteration)
-      gl.FLOAT,     // type of data in buffer
-      false,        // normalize
-      0,            // stride (0 = compute from size and type above)
-      0,            // offset in buffer
-  );
-
-  // setup matrixes, one per instance
-  this.numInstances = 5;
-  // make a typed array with one view per matrix
-  this.matrixData = new Float32Array(this.numInstances * 16)!;
-   for (let i = 0; i < this.numInstances; ++i) {
-    const byteOffsetToMatrix = i * 16 * 4;
-    const numFloatsForView = 16;
-    this.matrices.push(new Float32Array(
-        this.matrixData.buffer,
-        byteOffsetToMatrix,
-        numFloatsForView));
-  }
-
-  this.matrixBuffer = gl.createBuffer()!;
-  gl.bindBuffer(gl.ARRAY_BUFFER, this.matrixBuffer);
-  // just allocate the buffer
-  gl.bufferData(gl.ARRAY_BUFFER, this.matrixData.byteLength, gl.DYNAMIC_DRAW);
-
-  // set all 4 attributes for matrix
-  const bytesPerMatrix = 4 * 16;
-  for (let i = 0; i < 4; ++i) {
-    const loc = matrixLoc + i;
-    gl.enableVertexAttribArray(loc);
-    // note the stride and offset
-    const offset = i * 16;  // 4 floats per row, 4 bytes per float
+    // setup the position attribute
+    gl.enableVertexAttribArray(positionLoc);
     gl.vertexAttribPointer(
-        loc,              // location
-        4,                // size (num values to pull from buffer per iteration)
-        gl.FLOAT,         // type of data in buffer
-        false,            // normalize
-        bytesPerMatrix,   // stride, num bytes to advance to get to next set of values
-        offset,           // offset in buffer
+        positionLoc,  // location
+        2,            // size (num values to pull from buffer per iteration)
+        gl.FLOAT,     // type of data in buffer
+        false,        // normalize
+        0,            // stride (0 = compute from size and type above)
+        0,            // offset in buffer
     );
+
+    // setup matrixes, one per instance
+    this.numInstances = 6;
+    // make a typed array with one view per matrix
+    this.matrixData = new Float32Array(this.numInstances * 16)!;
+    for (let i = 0; i < this.numInstances; ++i) {
+      const byteOffsetToMatrix = i * 16 * 4;
+      const numFloatsForView = 16;
+      this.matrices.push(new Float32Array(
+          this.matrixData.buffer,
+          byteOffsetToMatrix,
+          numFloatsForView));
+    }
+
+    this.matrixBuffer = gl.createBuffer()!;
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.matrixBuffer);
+    // just allocate the buffer
+    gl.bufferData(gl.ARRAY_BUFFER, this.matrixData.byteLength, gl.DYNAMIC_DRAW);
+
+    // set all 4 attributes for matrix
+    const bytesPerMatrix = 4 * 16;
+    for (let i = 0; i < 4; ++i) {
+      const loc = matrixLoc + i;
+      gl.enableVertexAttribArray(loc);
+      // note the stride and offset
+      const offset = i * 16;  // 4 floats per row, 4 bytes per float
+      gl.vertexAttribPointer(
+          loc,              // location
+          4,                // size (num values to pull from buffer per iteration)
+          gl.FLOAT,         // type of data in buffer
+          false,            // normalize
+          bytesPerMatrix,   // stride, num bytes to advance to get to next set of values
+          offset,           // offset in buffer
+      );
+      // this line says this attribute only changes for each 1 instance
+      gl.vertexAttribDivisor(loc, 1);
+    }
+
+    // setup colors, one per instance
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,
+        new Float32Array([
+            1, 0, 0, 0.5,  // red
+            0, 1, 0, 1,  // green
+            0, 0, 1, 1,  // blue
+            1, 0, 1, 1,  // magenta
+            0, 1, 1, 1,  // cyan
+            0, 1, 0, 0.5,  // cyan
+          ]),
+        gl.STATIC_DRAW);
+
+    // set attribute for color
+    gl.enableVertexAttribArray(colorLoc);
+    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
     // this line says this attribute only changes for each 1 instance
-    gl.vertexAttribDivisor(loc, 1);
-  }
+    gl.vertexAttribDivisor(colorLoc, 1);
 
-  // setup colors, one per instance
-  const colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER,
-      new Float32Array([
-          1, 0, 0, 1,  // red
-          0, 1, 0, 1,  // green
-          0, 0, 1, 1,  // blue
-          1, 0, 1, 1,  // magenta
-          0, 1, 1, 1,  // cyan
-        ]),
-      gl.STATIC_DRAW);
-
-  // set attribute for color
-  gl.enableVertexAttribArray(colorLoc);
-  gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
-  // this line says this attribute only changes for each 1 instance
-  gl.vertexAttribDivisor(colorLoc, 1);
-
-//  requestAnimationFrame(()=>this.drawScene(0.0));
   }
 
   drawScene(gl: WebGL2RenderingContext,cam: camhandler.Camera, time: number) 
@@ -206,7 +190,7 @@ initScene(gl: WebGL2RenderingContext, cap: scene.TAnimation1Parameters,  p: twgl
     // Tell WebGL how to convert from clip space to pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    gl.useProgram(this.program!);
+  //  gl.useProgram(this.program!);
 
     // set the view and projection matrices since
     // they are shared by all instances
@@ -226,15 +210,13 @@ initScene(gl: WebGL2RenderingContext, cap: scene.TAnimation1Parameters,  p: twgl
     // upload the new matrix data
     gl.bindBuffer(gl.ARRAY_BUFFER, this.matrixBuffer!);
     gl.bufferSubData(gl.ARRAY_BUFFER, 0, this.matrixData!);
-
+//console.log("draw "+this.numInstances);
     gl.drawArraysInstanced(
       gl.TRIANGLES,
       0,             // offset
       this.numVertices,   // num vertices per instance
       this.numInstances,  // num instances
     );
-    //requestAnimationFrame(render);
-  //  requestAnimationFrame((time)=>this.drawScene(time));
   }
  
 }
