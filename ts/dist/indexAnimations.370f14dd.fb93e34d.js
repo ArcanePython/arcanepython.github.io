596bc2cca62d9181ea69ebb6f3cda4feab997895
ArcanePython <81058956+ArcanePython@users.noreply.github.com>
2023-01-04 16:00:27 +0100

    Luca42
    Luca42

--- ts/dist/indexAnimations.370f14dd.fb93e34d.js
+++ ts/dist/indexAnimations.370f14dd.fb93e34d.js
@@ -30598,7 +30598,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64072" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64512" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
